c1185896fbab2df4433c32db369d53dde0834363
Himanshu Yadav <155798146+himaanshuuyadav@users.noreply.github.com>
2025-04-16 11:44:27 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ import json
 import yt_dlp
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
-from .youtube_downloader import get_video_qualities_api, download_youtube_video, extract_video_id
+from youtube_downloader import get_video_qualities_api, download_youtube_video, extract_video_id
 
 app = Flask(__name__, static_folder="../frontend", static_url_path="/")
 CORS(app)
