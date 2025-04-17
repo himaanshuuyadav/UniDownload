6cd6af2707284e52490f9929a90836ec44a4e2c4
Himanshu Yadav <155798146+himaanshuuyadav@users.noreply.github.com>
2025-04-18 02:51:32 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -161,5 +161,6 @@ def download_video():
 # Run the server
 if __name__ == '__main__':
     import os
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    port = os.getenv("PORT", 8080)
+    app.run(host="0.0.0.0", port=port)
 
